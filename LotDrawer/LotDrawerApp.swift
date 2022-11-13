0bc5e822c4b5681ab0b1cde231eea38b18678efe
Jiaming Guo <j23guo@gmail.com>
2022-11-13 16:36:38 -0500

    Initial Commit

---
+++ LotDrawer/LotDrawerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LotDrawerApp.swift
+//  LotDrawer
+//
+//  Created by Jiaming Guo on 2022-11-13.
+//
+
+import SwiftUI
+
+@main
+struct LotDrawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
